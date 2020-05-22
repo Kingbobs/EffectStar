77bbe21da130607e34e0c55a61541552bd977f7a
TTaco <50176207+TWRRR@users.noreply.github.com>
2020-05-22 08:10:58 -0500

    fix

--- src/TTqco/EffectStar/Main.php
+++ src/TTqco/EffectStar/Main.php
@@ -2,7 +2,7 @@
 
 namespace TTqco\Main;
 # i didnt know what to use so i just keeps stacking em on
-use pocketmine\plugin\PluginBase as PB
+use pocketmine\plugin\PluginBase as PB;
 use pocketmine\event\Listener as L;
 use pocketmine\Player;
 use pocketmine\event\Listener;
